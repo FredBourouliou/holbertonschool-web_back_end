f880f1206ff51dff592ed1da3ecf3523d70ae518
Frederic Bourouliou <bourouliou@gmail.com>
2025-03-26 14:33:08 +0100

    update files 10 + 100

---
+++ ES6_classes/100-evcar.js
@@ -0,0 +1,14 @@
+import Car from './10-car.js';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    const clone = new Car();
+    return clone;
+  }
+}
+
