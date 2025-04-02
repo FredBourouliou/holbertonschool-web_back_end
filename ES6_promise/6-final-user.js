a3b94b90094dfb8e1716c8482f4ace0e4d349066
Frederic Bourouliou <bourouliou@gmail.com>
2025-04-02 17:54:16 +0200

    fix file 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,12 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([
-    signUpUser(firstName, lastName)
-      .then((value) => ({ status: 'fulfilled', value }))
-      .catch((error) => ({ status: 'rejected', value: error })),
-    uploadPhoto(fileName)
-      .then((value) => ({ status: 'fulfilled', value }))
-      .catch((error) => ({ status: 'rejected', value: error })),
-  ]);
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
