31c233752e96b15c83548a6d46976d26179d3c02
Frederic Bourouliou <bourouliou@gmail.com>
2025-04-22 16:00:16 +0200

    add file 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module for adding two floats."""
+
+
+def add(a: float, b: float) -> float:
+    """Add two floats and return their sum.
+
+    Args:
+        a: First float.
+        b: Second float.
+
+    Returns:
+        Sum of a and b.
+    """
+    return a + b 
