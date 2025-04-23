a4f7746cbe1422310cc2e925fb3de81dc7f11099
Frederic Bourouliou <bourouliou@gmail.com>
2025-04-23 09:15:10 +0200

    add files 2 ==> 4

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module for calculating the floor of a float."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Get the floor of a float.
+
+    Args:
+        n: Float to floor.
+
+    Returns:
+        Floor of the float as an integer.
+    """
+    return math.floor(n) 
---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module for converting a float to a string."""
+
+
+def to_str(n: float) -> str:
+    """Convert a float to a string.
+
+    Args:
+        n: Float to convert.
+
+    Returns:
+        String representation of the float.
+    """
+    return str(n) 
